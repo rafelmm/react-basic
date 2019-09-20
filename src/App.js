da6b1867827aa354c8ecf47aea896adecef13475
Rafel <rafelmmgmail.com>
2019-09-20 23:02:00 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import './App.css';
 
 function App() {
   return (
-    <Container >
-      <h1 className="header">Welcome Rafel</h1>
-    </Container>
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">RafelMM</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Container >
+        <h1 className="header">Welcome Rafel</h1>
+      </Container>
+    </>
   );
 }
 
