acb70e1687b275741c70fc9156ef1c8bf0471b8d
Rafel <rafelmmgmail.com>
2019-10-07 23:37:50 +0200

    elevo estado menuActive para poder esconder menu con scroll de la página y cambio estilo del menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,50 @@
-import React from 'react';
-
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React, {useState, useEffect} from 'react';
+import {Element} from 'react-scroll';
 import Menu from './menu/Menu';
 import Section from './section/Section';
 
 import './App.css';
 
-function Backup() {
-  return (
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#about">Sobre mi</Nav.Link>
-        <Nav.Link href="#contact">Contacto</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-
-  </Navbar>
-  );
-}
-
 function App() {
   const menuItems = [
     {
-      href: '#about',
+      target: 'about',
       text:'Sobre mi'
     },
     {
-      href: '#career',
+      target: 'career',
       text:'Experiencia'
     },
     {
-      href: '#contact',
+      target: 'contact',
       text:'Contacto'
     }
   ];
+  const [menuActive, setMenuActive] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
+
+  function handleScroll (event){
+    if ( window.scrollY > 10 && Math.abs(scrollY - window.scrollY)>10 ){
+      setMenuActive(false);
+    }
+    setScrollY(window.scrollY);
+  }
+
+  function handleMenuClick(){
+    console.log(menuActive);
+    setMenuActive(!menuActive);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  });
+
   return (
     <>
-      <Menu fixed='top' menuItems={menuItems}/>
+      <Menu menuItems={menuItems} menuActive={menuActive} onMenuClick={handleMenuClick}/>
       <div className="welcome-container">
         <div className="welcome-message">Hola, ¿quieres conocerme?</div>
         <div className="welcome-name">RAFEL MORMENEO</div>
@@ -47,39 +52,45 @@ function App() {
       </div>
       
       <div className="main-container">
-        <Section id="about" title="Este soy yo">
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum 
-        </Section>
-        <Section id="career" title="Mi experiencia">
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-          Lorem ipsum 
-        </Section>
-        <Section id="contact" title="Contacta conmigo">
+        <Element name="about">
+          <Section id="about" title="Este soy yo">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum 
+          </Section>
+        </Element>
+        <Element name="career">
+          <Section id="career" title="Mi experiencia">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum 
+          </Section>
+        </Element>
+        <Element name="contact">
+          <Section id="contact" title="Contacta conmigo">
 
-        </Section>
+          </Section>
+        </Element>
       </div>
     </>
   );
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,15 +1,26 @@
 import React, {useState} from 'react';
+import {Link} from 'react-scroll';
 import MenuButton from './MenuButton';
 
-const Menu = ({menuItems}) => {
-    const [menuActive, setMenuActive] = useState();
-
+const Menu = ({menuItems, menuActive, onMenuClick}) => {
     return (
         <div className="menu">
-            <MenuButton menuActive={menuActive} handleClick={() => setMenuActive(!menuActive)}/>
+            <MenuButton menuActive={menuActive} handleClick={onMenuClick}/>
             <div className={(menuActive?"active ":"") + "menu-items-container"}>
                 <ul>
-                    {menuItems.map(i => <li><a href={i.href} onClick={()=>setMenuActive(false)}>{i.text}</a></li>)}
+                    {menuItems.map(i => 
+                        <li key={i.target.toString()}>
+                            <Link 
+                                activeClass="active"
+                                to={i.target} 
+                                spy={true}
+                                smooth={true}
+                                hashSpy={true}
+                                duration={500} 
+                            >
+                                {i.text}
+                            </Link>
+                        </li>)}
                 </ul>
             </div>
         </div>
